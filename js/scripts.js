82ec78477081de0faa9ff69ea9dd20956d411c74
David Quisenberry and philip putnam <philip@>
2017-01-09 09:38:31 -0800

    add sub, mult, and divide functions to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,26 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
+// subtraction function
+
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
+
+// multiplication function
+
+var mult = function(number1, number2) {
+  return number1 * number2;
+};
+
+// division function
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2);
+var result = sub(number1, number2);
+alert(result);
