be7d9f19012841a6b3bc0036394bc5468413d1b2
David Quisenberry and philip putnam <philip@>
2017-01-09 09:52:12 -0800

    add BMI calculator functions

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,18 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = sub(number1, number2);
 alert(result);
+
+
+// BMI Calculator
+var bmiCalc = function(height, weight){
+  var bmi = (weight * 703) / (height**2);
+  return bmi;
+};
+
+var bmiDisplay = function(){
+  var yourHeight = parseInt(prompt("What is your height in inches?"));
+  var yourWeight = parseInt(prompt("What is your weight in pounds to the nearest pound?"));
+  alert("Your BMI is " + bmiCalc(yourHeight, yourWeight));
+}
+
+bmiDisplay();
