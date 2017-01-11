9e4b8b49ae38649f4fd55e0c8bd0e0adbb50fb89
Philip Putnam <putnamp989@gmail.com>
2017-01-11 11:12:44 -0800

    add jquery, form, and user interface html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+// Business (or back-end) logic:
 // addition function
 
 var add = function(number1, number2) {
@@ -6,13 +8,13 @@ var add = function(number1, number2) {
 
 // subtraction function
 
-var sub = function(number1, number2) {
+var subtract = function(number1, number2) {
   return number1 - number2;
 };
 
 // multiplication function
 
-var mult = function(number1, number2) {
+var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
@@ -22,44 +24,49 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
+// // BMI Calculator
+// var bmiCalc = function(height, weight){
+//   var bmi = (weight * 703) / (height**2);
+//   return bmi;
+// };
+//
+// var bmiDisplay = function(){
+//   var yourHeight = parseInt(prompt("What is your height in inches?"));
+//   var yourWeight = parseInt(prompt("What is your weight in pounds to the nearest pound?"));
+//   alert("Your BMI is " + bmiCalc(yourHeight, yourWeight));
+// }
+//
+// bmiDisplay();
+//
+// // fahrenheit to celsius
+//
+// var fahrenToCel = function(temp) {
+//   var newTemp = ((temp - 32) * 5) / 9;
+//   return newTemp;
+// };
+//
+//
+// // celsius to fahrenheit
+//
+// var celToFahren = function(temp) {
+//   var newTemp = ((temp * 9) / 5) +32;
+//   return newTemp;
+// };
+//
+// var usrFahrenTemp = celToFahren(parseInt(prompt("What is the temp in Celsius?")));
+// var usrCelciusTemp = fahrenToCel(parseInt(prompt("What is the temp in Fahrenheit?")));
+//
+// alert(usrFahrenTemp);
+// alert(usrCelciusTemp);
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = sub(number1, number2);
-alert(result);
+// Front-end or user interface logic here:
 
-
-// BMI Calculator
-var bmiCalc = function(height, weight){
-  var bmi = (weight * 703) / (height**2);
-  return bmi;
-};
-
-var bmiDisplay = function(){
-  var yourHeight = parseInt(prompt("What is your height in inches?"));
-  var yourWeight = parseInt(prompt("What is your weight in pounds to the nearest pound?"));
-  alert("Your BMI is " + bmiCalc(yourHeight, yourWeight));
-}
-
-bmiDisplay();
-
-// fahrenheit to celsius
-
-var fahrenToCel = function(temp) {
-  var newTemp = ((temp - 32) * 5) / 9;
-  return newTemp;
-};
-
-
-// celsius to fahrenheit
-
-var celToFahren = function(temp) {
-  var newTemp = ((temp * 9) / 5) +32;
-  return newTemp;
-};
-
-var usrFahrenTemp = celToFahren(parseInt(prompt("What is the temp in Celsius?")));
-var usrCelciusTemp = fahrenToCel(parseInt(prompt("What is the temp in Fahrenheit?")));
-
-alert(usrFahrenTemp);
-alert(usrCelciusTemp);
+$(document).ready(function() {
+  $("form#add").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
