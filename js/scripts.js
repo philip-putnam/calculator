015d49063ec0688494e9051bb9720a123e54d569
philip putnam <putnamp989@gmail.com>
2017-01-12 17:20:54 -0800

    add jquery branching if-else for operator selection

--- js/scripts.js
+++ js/scripts.js
@@ -62,37 +62,60 @@ var divide = function(number1, number2) {
 // Front-end or user interface logic here:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event){
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+
+    $("#output").text(result)
+
   });
-
-  $("form#subtract").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#sub1").val());
-    var number2 = parseInt($("#sub2").val());
-    var result = subtract(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#mult1").val());
-    var number2 = parseInt($("#mult2").val());
-    var result = multiply(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#divide1").val());
-    var number2 = parseInt($("#divide2").val());
-    var result = divide(number1, number2);
-    $("#output").text(result);
-  });
-
-
 });
+
+
+
+  // $("form#add").submit(function(event){
+  //   event.preventDefault();
+  //   var number1 = parseInt($("#add1").val());
+  //   var number2 = parseInt($("#add2").val());
+  //   var result = add(number1, number2);
+  //   $("#output").text(result);
+  // });
+  //
+  // $("form#subtract").submit(function(event){
+  //   event.preventDefault();
+  //   var number1 = parseInt($("#sub1").val());
+  //   var number2 = parseInt($("#sub2").val());
+  //   var result = subtract(number1, number2);
+  //   $("#output").text(result);
+  // });
+  //
+  // $("form#multiply").submit(function(event) {
+  //   event.preventDefault();
+  //   var number1 = parseInt($("#mult1").val());
+  //   var number2 = parseInt($("#mult2").val());
+  //   var result = multiply(number1, number2);
+  //   $("#output").text(result);
+  // });
+  //
+  // $("form#divide").submit(function(event) {
+  //   event.preventDefault();
+  //   var number1 = parseInt($("#divide1").val());
+  //   var number2 = parseInt($("#divide2").val());
+  //   var result = divide(number1, number2);
+  //   $("#output").text(result);
+  // });
+
+
+// });
